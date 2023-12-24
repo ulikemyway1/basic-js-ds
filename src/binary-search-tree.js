cb30a3e0173e2312a5958fcf85008712b4fcbd5d
Alex_Nout <calium_by@mail.ru>
2023-12-24 14:27:44 +0300

    feat: implement add method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -9,18 +9,42 @@ const { Node } = require('../extensions/list-tree.js');
 class BinarySearchTree {
 
   root() {
-    if (this.data) {
-      return this.data;
+    if (this.rootData) {
+      return this.rootData;
     } else {
       return null;
     }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  add(data) {
+    if (!this.rootData) {
+      this.rootData = new Node(data);
+    } else {
+      const newNode = new Node(data);
+      let nextNode;
+      let currentNode = this.rootData;
+      if (data > currentNode.data) {
+        nextNode = currentNode.right;
+      } else {
+        nextNode = currentNode.left;
+      }
+    while (nextNode) {
+      currentNode = nextNode;
+      if (data > nextNode.data) {
+        nextNode = nextNode.right;
+      } else {
+        nextNode = nextNode.left;
+      }
+    }
 
+    if (data > currentNode.data) {
+      currentNode.right = newNode;
+    } else {
+      currentNode.left = newNode;
+    }
+
+  }
+  }
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
@@ -47,6 +71,13 @@ class BinarySearchTree {
   }
 }
 
+
+const tree = new BinarySearchTree();
+      tree.add(2);
+      tree.add(4);
+      tree.add(6);
+    console.log(tree)
+console.log(tree.root())
 module.exports = {
   BinarySearchTree
 };
